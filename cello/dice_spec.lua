dd0db008abdc1ea5659e87613db530ab9481f948
Trevor Redfern <trevorredfern@gmail.com>
2019-04-12 06:52:24 -0700

    Initial commit for builds

---
+++ cello/dice_spec.lua
@@ -0,0 +1,9 @@
+-- Copyright (c) 2019 Trevor Redfern
+--
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
+describe("Dice", function()
+  it("passes a test", function()
+  end)
+end)
